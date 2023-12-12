6b8683f4b413a2686567ea1302d2b8147a90755c
purnima11apr <purnimachdry@gmail.com>
2023-12-12 18:34:45 +0530

    submitting code-Tue Dec 12 2023 18:34:44 GMT+0530 (India Standard Time)

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,7 +4,6 @@ import React, {useState} from "react";
 const Component = () =>{
   return(
     <div>
-       <h1>React Component</h1>
       <p>I am learning React. My life is getting better.</p>
     </div>
   )
