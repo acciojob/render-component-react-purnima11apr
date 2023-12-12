a7b5e00513dfd36b9438fc2cab192a9fc3c6cddf
purnima11apr <purnimachdry@gmail.com>
2023-12-12 18:31:38 +0530

    submitting code-Tue Dec 12 2023 18:31:37 GMT+0530 (India Standard Time)

---
+++ src/components/Component.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+
+const Component = () =>{
+  return(
+    <div>
+       <h1>React Component</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  )
+}
+
+export default Component;
